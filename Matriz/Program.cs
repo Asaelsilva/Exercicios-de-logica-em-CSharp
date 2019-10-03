b25d5ee2529537a7cfbfbde032557f41cae1dc0d
Asael <asael.guitar_g3@hotmail.com>
2019-10-03 20:31:10 -0300

    Segunda estrutura for da matriz

--- Matriz/Program.cs
+++ Matriz/Program.cs
@@ -15,11 +15,33 @@ namespace Matriz
 
             int[,] matriz;
 
-            linha = int.Parse(Console.ReadLine());
-            coluna = int.Parse(Console.ReadLine());
-            matriz = [linha, coluna];
+            string[] s1 = Console.ReadLine().Split(' ');
+            linha = int.Parse(s1[0]);
+            coluna = int.Parse(s1[1]);
+            matriz = new int[linha, coluna];
 
 
+            for (int i = 0; i < linha; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < coluna; j++)
+                {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            for (int i = 0; i < linha; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
