b2be1066187f786f748a72d53ad665ea4e0b69fd
Asael <asael.guitar_g3@hotmail.com>
2019-10-03 20:08:46 -0300

    Declarando variaveis para Matriz

---
+++ Matriz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int linha, coluna;
+
+            int[,] matriz;
+
+            linha = int.Parse(Console.ReadLine());
+            coluna = int.Parse(Console.ReadLine());
+            matriz = [linha, coluna];
+
+
+        }
+    }
+}
